f1f00efa4817d7a8140f4c5b5427493966519f7b
Jonny Hofmann <jonny.hofmann@divtec.ch>
2016-04-19 10:34:10 +0200

    static routes

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 // Server for the jump and run game
-var app = require('express')();
+
+// Modules
+var express = require("express");
+var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var fs = require("fs");
+var path = require("path");
 
 // Read the config file and start the server
 fs.readFile("config.json", "utf8", (err, data) => {
@@ -17,11 +21,14 @@ fs.readFile("config.json", "utf8", (err, data) => {
   }
 });
 
-
+// Static routes
+app.use("/js", express.static("js"));
+app.use("/maps", express.static("maps"));
+app.use("/img", express.static("img"));
 
 // Routings
 app.get("/", (req, res) => {
-  res.send("index.html");
+  res.sendFile(path.join( __dirname+ "/index.html"));
 });
 io.on("connection", (data) => {
   console.log("Client connected");
