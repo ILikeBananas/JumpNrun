173d9ee96749c379f00ec68229acba0d62c37cd8
Jonny Hofmann <jonny.hofmann@divtec.ch>
2016-04-25 08:09:06 +0200

    Start score
    addScore in server.js
    change port to 8080
    local socket.io for client
    add colog and fade modules
    create scoretest.jade

--- js/clientSocket.js
+++ js/clientSocket.js
@@ -1,5 +1,19 @@
 var socket = io();
 //io.emit("connection", "");
-socket.on("test", () => {
-  console.log("a client connected");
+socket.on("test", (data) => {
+  console.log("a client connecte");
 });
+socket.on("clicked", () => {
+  console.log("Server confirme que tu as cliquer dur");
+  newScore();
+});
+
+
+socket.on("updateScore", (data) => {
+  console.log("nouveau score est arrivé");
+  console.log(data);
+})
+function newScore () {
+  console.log("tu m'as cliquer dur");
+  socket.emit("newScore", {"name":"Max", "score":"500"});
+}
--- server.js
+++ server.js
@@ -3,43 +3,73 @@
 // Modules
 var express = require("express");
 var app = express();
-var server = require('http').Server(app);
+var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var fs = require("fs");
 var path = require("path");
+var colog = require("colog");
+var config = require("./config/config.json");
 
 
   var connects = 0;
-// Read the config file and start the server
-fs.readFile("config/config.json", "utf8", (err, data) => {
-  // Test if the read was succesfull
-  if(err){
-    console.log("Config file read error" + err);
+  if(config.port){
+    server.listen(config.port);
+    colog.success("Server started on port " + config.port);
   } else {
-    data = JSON.parse(data);
-    // Start the server
-    server.listen(data["port"]);
-    console.log("Server started on port %d", data["port"]);
+    colog.error("server port is not defined");
+    colog.error("Programm stopped");
+    process.exit(1);
   }
-});
 
 // Static routes
-app.use("/js", express.static("js"));
-app.use("/maps", express.static("maps"));
-app.use("/img", express.static("img"));
-app.use("/css", express.static("css"));
+app.use("/js",    express.static("js"));
+app.use("/maps",  express.static("maps"));
+app.use("/img",   express.static("img"));
+app.use("/css",   express.static("css"));
 
 // Routings
 app.get("/", (req, res) => {
   res.sendFile(path.join( __dirname+ "/index.html"));
 });
-io.on("connection", () => {
-  connects++;
-  process.stdout.clearLine();
-  process.stdout.cursorTo(0);
-  process.stdout.write(connects + " Client connected");
+app.get("/score", (req, res) => {
+  app.set("view engine", "jade");
+  res.render("scoretest.jade");
+})
 
-  io.on("newScore", (data) => {
-    io.emit("newHighscore", data);
+io.on("connection", () => {
+  console.log("client connected");
+  io.on("newScore", (score, name) => {
+    console.log("newscore")
+    var allScore = addScore(score);
+    colog.dump(score);
+    io.emit("updateScore", score);
   });
 });
+
+// Add a new score to the scores json
+// Retun the complete scoreboard
+function addScore (score){
+  data = readJSON("config/highscores.json");
+  colog.cyan(data[3].name);
+  data.scores[data.viewers.length + 1].name = score.name;
+  data.scores[data.viewers.length + 1].score = score.score;
+  var json = JSON.stringify(score);
+  // Update json file
+  fs.writeFile("config/highscores.json", score, (err) => {
+    if (err) throw err;
+    colog.success("Scores has been saved!");
+  });
+  return score;
+}
+
+function readJSON (fileName) {
+  fs.readFile(fileame, "utf8", (err, data) => {
+    data = JSON.parse(data);
+    if(err){
+      colog.error(err);
+      throw err;
+    } else {
+      return data;
+    }
+  });
+}
