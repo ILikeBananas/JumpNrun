33e02cc2e72ea60895ea7e3fbe144777d8027f3e
Jonny Hofmann <jonny.hofmann@divtec.ch>
2016-06-13 13:35:30 +0200

    Add jump/crouch

--- js/kinect.js
+++ js/kinect.js
@@ -6,43 +6,50 @@
 const LIMIT_RIGHT = 200;
 const LIMIT_LEFT = -100;
 
-  var radar = {
-              onuserfound: function (user) {
-                  document.getElementById("clientSeen").style.backgroundColor = "green";
-              },
-              onuserlost: function (user) {
-                  document.getElementById("clientSeen").style.backgroundColor = "red";
-              },
-              ondataupdate: function (zigdata) {
-                  for (var userid in zigdata.users) {
-                      var user = zigdata.users[userid];
-                      var pos = user.position;
-                      console.log(pos[0]);
-                      if(pos[0] > LIMIT_RIGHT) {
-                        // right
-                        roadPath = 1;
-                      } else if (pos[0] < LIMIT_LEFT) {
-                        //left
-                        roadPath = -1;
-                      } else {
-                        //center
-                        roadPath = 0;
-                      }
-                  }
-              }
-          };
-          // By adding the radar object as a listener to the zig object, the zig object will now start calling
-          // the callback functions defined in the radar object.
-          zig.addListener(radar);
+// Left Center Right detecion
+var radar = {
+  ondataupdate: function (zigdata) {
+    if (zigdata.users.length != 0){
+      for (var userid in zigdata.users) {
+        var user = zigdata.users[userid];
+        var pos = user.position;
+        if(pos[0] > LIMIT_RIGHT) {
+          console.log("right");
+          goToPath(1);
+        } else if (pos[0] < LIMIT_LEFT) {
+          console.log("left");
+          goToPath(-1);
+        } else {
+          console.log("center");
+          goToPath(0);
+        }
+      }
+    } else {
+      console.log("erreur trouvé");
+    }
+  }
+};
 
+zig.addListener(radar);
 
+// Jump and crouch detection
+var engager = zig.EngageUsersWithSkeleton(1);
+engager.addEventListener('userengaged', function(user) {
+  console.log('User engaged: ' + user.id);
 
+  user.addEventListener('userupdate', function(user) {
+    var headHight = user.skeleton[zig.Joint.Head].position[1];
 
-
-    // <<< INSÉRER LE CODE ICI ! (execution à chaque frame)
-    //   roadPath = -1 -> chemin à gauche
-    //   roadPath = 0  -> chemin au centre
-    //   roadPath = 1  -> chemin à droite
-    //   Déplacements : moveLeft(), moveRight(),
-    //   jump() pour sauter et squat() pour se baisser
-    // >>>
+    if(headHight > 800){
+      jump();
+      console.log("jump");
+    } else if (headHight < 0){
+      squat();
+      console.log("squat");
+    }
+  });
+});
+engager.addEventListener('userdisengaged', function(user) {
+  console.log('User disengaged: ' + user.id);
+});
+zig.addListener(engager);
