5fd3d2d744d5c9344b2695d805a07cbc04769b16
Jonny Hofmann <jonny.hofmann@divtec.ch>
2016-08-26 08:44:58 +0200

    start score
    new index
    links

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
 // Server for the jump and run game
 
 // Modules
-var express = require("express");
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var fs = require("fs");
-var path = require("path");
-var config = require("./config/config.json");
-var util = require("util");
+var express =   require("express");
+var app =       express();
+var server =    require('http').Server(app);
+var io =        require('socket.io')(server);
+var fs =        require("fs");
+var path =      require("path");
+var jsonFile =  require("jsonfile");
+var config =    require("./config/config.json");
+var util =      require("util");
 
 server.listen(config.port);
 console.log("server started " +  config.port);
 
 // Static routes
-app.use("/js",    express.static("js"));
-app.use("/maps",  express.static("maps"));
-app.use("/img",   express.static("img"));
-app.use("/css", express.static("css"));
+app.use("/js",      express.static("js"));
+app.use("/maps",    express.static("maps"));
+app.use("/img",     express.static("img"));
+app.use("/css",     express.static("css"));
 app.use("/vendors", express.static("vendors"));
-app.use("/obj", express.static("obj"));
-app.use("/levels", express.static("levels"));
+app.use("/obj",     express.static("obj"));
+app.use("/levels",  express.static("levels"));
 
 // Set the view engine
 app.set("view engine", "pug");
@@ -36,6 +37,7 @@ app.get("/game", (req, res) => {
   res.render("game")
 });
 
+// socket to receive the scores
 io.on("connection", (socket) => {
   console.log("Client connected");
   socket.on("newScore", (user) => {
