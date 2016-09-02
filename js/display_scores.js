8669a792188d1d8b212e69330d34a5599c913fb3
Chappuis25 <sebastien.chappuis@divtec.ch>
2016-09-02 13:16:11 +0200

    Suppression de la possibilité d'injecter du HTML

--- js/display_scores.js
+++ js/display_scores.js
@@ -4,6 +4,7 @@
 
 var scores = {};
 
+// Charge le fichier "scores.json"
 $.ajax('scoresList')
     .done(function(file) {
         scores = file.scores;
@@ -17,13 +18,20 @@ $.ajax('scoresList')
 });
 
 
-
+// Affiche le score
 function displayScores() {
     for (var i = 0; i < Math.min(10, scores.length); i++) {
-        $('#scores').children('table').append('<tr><td>' + scores[i].name +
-                                              '</td><td>' + scores[i].score +
-                                              '</td><td>' + scores[i].distance +
-                                              '</td><td>' + scores[i].coins +
-                                              '</td></tr>')
+        
+        var tr = $('#scores').children('table').append('<tr>').children('tbody').children('tr');
+        tr = $($(tr)[tr.length - 1]);
+        
+        for (var j = 0; j < 4; j++) {
+            tr.append('<td></td>');
+        }
+        
+        $(tr.children('td')[0]).text(scores[i].name);
+        $(tr.children('td')[1]).text(scores[i].score);
+        $(tr.children('td')[2]).text(scores[i].distance);
+        $(tr.children('td')[3]).text(scores[i].coins);
     }
 }
